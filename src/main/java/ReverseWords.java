70bb05873d9f2b185f64837ffc25abf584a8455e
1Dz87 <daiz87@yandex.by>
2020-11-21 11:47:00 +0300

    ReverseWords

--- src/main/java/ReverseWords.java
+++ src/main/java/ReverseWords.java
@@ -12,16 +12,8 @@ public class ReverseWords {
     // should return "battle no requires which that is victory greatest The"
 
     public static String reverseWords(String str) {
-        List<String> reverseWords = new ArrayList<>(Arrays.asList(((str).split(" "))));
-        Collections.reverse(reverseWords);
-        StringBuffer result = new StringBuffer();
-        reverseWords.forEach(st -> {
-            if (reverseWords.indexOf(st) < (reverseWords.size() -1)) {
-                result.append(st).append(" ");
-            } else {
-                result.append(st);
-            }
-        });
-        return result.toString();
+        List Words = Arrays.asList(str.split(" "));
+        Collections.reverse(Words);
+        return String.join(" ", Words);
     }
 }
